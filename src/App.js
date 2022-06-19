7b9a7cc6d9a3065e8e3abebcae74f45e590d9068
Maximilian Seiderer <maximilian@seiderer.me>
2022-06-19 16:16:39 +0000

    fonts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Outlet } from "react-router-dom";
+import "./App.css";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='navbar'>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/calculator'>Calculator</Link>
+        </nav>
+        <Link to='#'>NL</Link>
+      </div>
+      <Outlet />
     </div>
   );
-}
+};
+
+export const Home = () => {
+  return (
+    <div>
+      <div className='hero'>
+        <h1>How the Dutch state calculates welfare things lorem ipsum</h1>
+      </div>
+      <div className='cta-buttons'></div>
+      <div className='credits'></div>
+    </div>
+  );
+};
+
+export const Calculator = () => {
+  return <div />;
+};
 
 export default App;
