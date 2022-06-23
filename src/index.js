6f341d4c44e46edf43b3cbfd9bce4ab8698505b3
Maximilian Seiderer <maximilian@seiderer.me>
2022-06-23 02:09:00 +0000

    gh-pages deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import { About, App, Home } from "./App";
 import { Calculator } from "./Calculator";
@@ -8,7 +8,7 @@ import { Calculator } from "./Calculator";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<App />}>
           <Route path='/' element={<Home />} />
@@ -16,6 +16,6 @@ root.render(
           <Route path='about' element={<About />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
